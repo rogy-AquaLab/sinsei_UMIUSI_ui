68517db525791458722042c9698928e7b9283dd5
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-11-11 09:33:55 +0900

    style: format

--- src/RosProvider.tsx
+++ src/RosProvider.tsx
@@ -9,10 +9,7 @@ import { Ros } from 'roslib'
 
 const RosContext = createContext<Ros | null>(null)
 
-const RosProvider = ({
-  children,
-  url,
-}: PropsWithChildren<{ url: string }>) => {
+const RosProvider = ({ children, url }: PropsWithChildren<{ url: string }>) => {
   const rosRef = useRef<Ros | null>(null)
   const [connected, setConnected] = useState<boolean>(false)
 
