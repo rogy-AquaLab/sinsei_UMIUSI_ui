7edabb0a7a92af00a5941fe49bc76259bc501dc3
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-12-05 18:10:58 +0900

    feat(ui): コントローラーの切り替えに対応、設定画面の微調整

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,6 +1,7 @@
 import type { PropsWithChildren } from 'react'
 import RosConnectButton from './RosConnectButton'
-import UrlInput from './UrlInput'
+import UrlInput from './drawer/UrlInput'
+import GamepadSelect from './drawer/GamepadSelect'
 
 const Drawer = ({ children }: PropsWithChildren) => {
   return (
@@ -17,7 +18,13 @@ const Drawer = ({ children }: PropsWithChildren) => {
         <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-80">
           {/* Sidebar content here */}
           <div className="is-drawer-close:hidden w-full p-3">
-            <UrlInput />
+            <fieldset className="fieldset bg-base-300 border-base-300 rounded-box border p-4">
+              <legend className="fieldset-legend">Config</legend>
+              <label className="label">rosbridge URL</label>
+              <UrlInput />
+              <label className="label">Gamepad</label>
+              <GamepadSelect />
+            </fieldset>
           </div>
           <ul className="menu w-full grow">
             {/* List item */}
---
+++ src/components/drawer/UrlInput.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react'
+import { RosContext } from '../../contexts/RosContext'
+
+const UrlInput = () => {
+  const { connectionState, url, setUrl } = useContext(RosContext)
+
+  return (
+    <input
+      type="url"
+      className="input"
+      required
+      placeholder="ws://"
+      value={url}
+      onChange={(e) => setUrl(e.target.value)}
+      disabled={connectionState !== 'disconnected'}
+    />
+  )
+}
+
+export default UrlInput
