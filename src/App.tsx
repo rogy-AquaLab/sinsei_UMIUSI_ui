e5a4f22f7be62e600690342046ae6e938d3111b7
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-12-05 16:47:53 +0900

    refactor(react): providerの入れ子を`main.tsx`に移動

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,34 @@
-import { ToastProvider } from './contexts/ToastContext'
-import { RosProvider } from './contexts/RosContext'
-import { GamepadProvider } from './contexts/GamepadContext'
 import NavBar from './components/NavBar'
 import Drawer from './components/Drawer'
 import CameraViewer from './components/CameraViewer'
 
 function App() {
   return (
-    <ToastProvider>
-      <RosProvider url="ws://umiusi2.local:9090">
-        <GamepadProvider>
-          <Drawer>
-            <div className="flex flex-col h-screen overflow-hidden">
-              <NavBar />
-              <div className="flex-1 min-h-0">
-                {/* <div className="flex flex-row w-full h-full">
-                <div className="flex-1 min-w-0 min-h-0"> */}
-                <CameraViewer
-                  hostname="http://umiusi2.local:8080"
-                  topicName="/pi_camera/image_raw"
-                  width={1024}
-                  height={768}
-                />
-                {/* </div>
-                <div className="flex-1 min-w-0 min-h-0">
-                  <CameraViewer
-                    hostname="http://umiusi2.local:8080"
-                    topicName="/usb_camera/image_raw"
-                    width={1024}
-                    height={768}
-                  />
-                </div>
-              </div> */}
-              </div>
-            </div>
-          </Drawer>
-        </GamepadProvider>
-      </RosProvider>
-    </ToastProvider>
+    <Drawer>
+      <div className="flex flex-col h-screen overflow-hidden">
+        <NavBar />
+        <div className="flex-1 min-h-0">
+          {/* <div className="flex flex-row w-full h-full">
+          <div className="flex-1 min-w-0 min-h-0"> */}
+          <CameraViewer
+            hostname="http://umiusi2.local:8080"
+            topicName="/pi_camera/image_raw"
+            width={1024}
+            height={768}
+          />
+          {/* </div>
+          <div className="flex-1 min-w-0 min-h-0">
+            <CameraViewer
+              hostname="http://umiusi2.local:8080"
+              topicName="/usb_camera/image_raw"
+              width={1024}
+              height={768}
+            />
+          </div>
+        </div> */}
+        </div>
+      </div>
+    </Drawer>
   )
 }
 
